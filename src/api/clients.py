be6a89d7ec029e6af5e595b05452bc23c824dc3e
Infinity080 <pawel.wojcik080@gmail.com>
2025-08-30 19:04:22 +0200

    fix redundant redis client

--- src/api/clients.py
+++ src/api/clients.py
@@ -2,16 +2,13 @@ import redis.asyncio as redis
 from qdrant_client.async_qdrant_client import AsyncQdrantClient as QdrantClient
 import os
 
-redis_client = redis.Redis(host="redis", port=6379, db=0)
-qdrant_client = QdrantClient(host="qdrant", port=6333)
-
 redis_client: redis.Redis = redis.Redis(
     host=os.getenv("REDIS_HOST", "localhost"),
     port=int(os.getenv("REDIS_PORT", 6379)),
     db=0
 )
 
-qdrant_clientL: QdrantClient = QdrantClient(
+qdrant_client: QdrantClient = QdrantClient(
     host=os.getenv("QDRANT_HOST", "localhost"),
     port=int(os.getenv("QDRANT_PORT", 6333))
 )
