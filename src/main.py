b922cd987c61b4eeb4a4dbf3e17d369abd1b8ac6
Infinity080 <pawel.wojcik080@gmail.com>
2025-08-31 01:16:36 +0200

    add embeddings and qdrant setup to a startup event in main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 from api.endpoints import router
-from api.clients import qdrant_client, redis_client
+from api.clients import qdrant_client, redis_client, setup_qdrant_collection
+from ai import AIService
 
 # setup
 app = FastAPI()
 app.include_router(router)
+
+@app.on_event("startup")
+async def main():
+    # health checks
+    from api.endpoints import healthz
+    await healthz()
+
+    # setup
+    await setup_qdrant_collection("main", 384, "COSINE")
+    ai = AIService("all-MiniLM-L6-v2")
+    embeddings = ai._generate_embeddings()
+
+    # save to qdrant
+    await ai.save_embeddings_to_qdrant("main", embeddings)
